cbe17b5fe9f2fafd2ced9b5df38f1c28997f9c25
Hao Chen <inbox@haochen.name>
2019-01-27 06:48:04 -0600

    skip files already downloaded

--- rotate_compile.sh
+++ rotate_compile.sh
@@ -5,20 +5,19 @@
 
 # change the following line to match your files.
 
-imgs=`ls chr20*bnboth.png`
-
-cnt=0
-
-for i in $imgs; do
-	cnt=$(( $cnt + 5 ))
-	cntd=`bc -l <<< " scale =1; $cnt / 10"`	
-	loc="$cntd\_MB"	
-	echo $loc
-	j=`echo $i |sed "s/png/jpg/"` 
-	echo $j
-	montage -rotate 45  -geometry 1200x1200+0+0 $i png:- |convert -  -crop  1170x1170+150+400 png:- |convert - -pointsize 46 -font Times-Bold -annotate +900+1100  `echo $loc`  crop_$j 
-done;
-
-## change the bn*jpg to match your files
-montage crop_*jpg -tile 12x6 -geometry 200x200-2+0  compiled_chr20.png
-
+for chrs in {1..8} ; do
+	chr="chr$chrs"
+	imgs=`ls $chr\_*bn_male.png`
+	cnt=0
+	for i in $imgs; do
+		cnt=$(( $cnt + 5 ))
+		cntd=`bc -l <<< " scale =1; $cnt / 10"`	
+		loc="$cntd\_MB"	
+		j=`echo $i |sed "s/png/jpg/"` 
+		echo $j
+		if [ ! -f crop_$j ] ; then 
+			montage -rotate 45  -geometry 1200x1200+0+0 $i png:- |convert -  -crop  1170x1170+150+400 png:- |convert - -pointsize 64 -font Times-Bold -annotate +900+1100  `echo $loc`  crop_$j 
+		fi
+	done;
+	montage crop_$chr\_*jpg -tile 10x4 -geometry 200x200-2+0  compiled_$chr\_bnmale.pdf
+done
--- save_matrixView_chr.py
+++ save_matrixView_chr.py
@@ -6,8 +6,8 @@ import sys
 import re
 import os
 import math
- 
-# save matrix view images for one chr
+
+# save matrix view images for all chr
 
 samples={'lewssnhsd':'bGV3c25oc2RfMDUwOF9sb3VwZS5sb3VwZQ==',
 'bn_both'  : 'Ym5fYm90aF9sb3VwZS5sb3VwZQ==',
@@ -21,7 +21,7 @@ samples={'lewssnhsd':'bGV3c25oc2RfMDUwOF9sb3VwZS5sb3VwZQ==',
 'bn_eve' :'Ym5fZXZhX2xvdXBlLmxvdXBl'
 }
 
-chrlen={ 'chr1': 282.76,
+chrlen={'chr1': 282.76,
 'chr2': 266.44,
 'chr3': 177.7,
 'chr4': 184.23,
@@ -43,51 +43,55 @@ chrlen={ 'chr1': 282.76,
 'chr20': 56.21,
 'chrX': 159.97,
 'chrY': 3.31,
-'MT': 0.02}
-
+'chrUn_random': 88.16,
+'chrMT': 0.02}
 
 strain='bn_male'
-
-downloadDir="/home/hao/Downloads/"
 serverRoot="http://hchen3:3000/loupe/view/"
-# disable the download dialog popup box
-options=Options()
-options.headless=True
-profile = webdriver.FirefoxProfile()
-profile.set_preference('browser.download.folderList', 2) # custom location
-profile.set_preference('browser.download.manager.showWhenStarting', False)
-profile.set_preference('browser.download.dir', downloadDir)
-profile.set_preference('browser.helperApps.neverAsk.saveToDisk', "image/png,image/jpeg")
 
-browser = webdriver.Firefox(firefox_profile=profile, options=options)
+def saveMVperChr(chr):
+    whichchr="chr"+str(chr)
+    baselength=chrlen[whichchr]*1000000
+    resolu=500000
+    images=math.ceil(baselength/resolu)
+    print (whichchr + ": total " + str(images) + " images")
+    browser = webdriver.Firefox(firefox_profile=profile, options=options)
+    for cnt in range(images):
+        xstart=cnt*resolu+1
+        xend=xstart+resolu-1
+        xchr=whichchr + ":"  + str(xstart) + "-" + whichchr + ":" + str(xend)
+        xy=xchr + ";" + xchr
+        filename=whichchr + "_" + str(xstart).zfill(11) + "-" + str(xend).zfill(11) + "_" + strain  + ".png"
+        if not os.path.isfile(filename):
+            print(str(cnt)+ " genomic region:" + xy )
+            xy=re.sub(r':', '%2B', xy)
+            xy=re.sub(r';', '&y=', xy)
+            url=serverRoot + samples[strain] + '/matrix?x='+ xy
+            #print (url)
+            browser.get(url)
+            time.sleep(10) # set it to allow the image to load into the browser
+            elem = browser.find_element_by_id('save_image_as_png')  # find the save button 
+            elem.click() # and click it
+            movecommand="mv " + downloadDir + "loupe-sv-barcode-matrix.png "  + filename
+            os.system(movecommand)
+        # somehow the browser fails to load after about 9 images and needs to be restarted
+        if (cnt%8==7):
+            browser.close() # this keeps the profile
+            browser = webdriver.Firefox(firefox_profile=profile, options=options)
+    browser.quit() # now delete the profile
 
-whichchr="chr19"
-chrlen=chrlen[whichchr]*1000000
-resolu=500000
-images=math.ceil(chrlen/resolu)
-print ("total " + str(images) + " images")
-
-for cnt in range(images):
-    xstart=cnt*resolu+1
-    xend=xstart+resolu-1
-    xchr=whichchr + ":"  + str(xstart) + "-" + whichchr + ":" + str(xend)
-    xy=xchr + ";" + xchr
-    print(str(cnt)+ " genomic region:" + xy )
-    filename=whichchr + "_" + str(xstart).zfill(11) + "-" + str(xend).zfill(11) + "_" + strain  + ".png"
-    xy=re.sub(r':', '%2B', xy)
-    xy=re.sub(r';', '&y=', xy)
-    url=serverRoot + samples[strain] + '/matrix?x='+ xy
-    #print (url)
-    browser.get(url)
-    time.sleep(10) # set it to allow the image to load into the browser
-    elem = browser.find_element_by_id('save_image_as_png')  # find the save button 
-    elem.click() # and click it
-    movecommand="mv " + downloadDir + "loupe-sv-barcode-matrix.png "  + filename
-    os.system(movecommand)
-    # somehow the browser fails to load after about 9 images and needs to be restarted
-    if (cnt%8==7):
-        browser.close() # this keeps the profile
-        browser = webdriver.Firefox(firefox_profile=profile, options=options)
-browser.quit() # now delete the profile
-
+#for i in range(1,21):
+for i in ["X", "Y", "Un_random"]:
+    downloadDir="/home/hao/Downloads/chr" +  str(i) + "/"
+    os.system("mkdir "+downloadDir)
+    # disable the download dialog popup box
+    options=Options()
+    options.headless=True
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference('browser.download.folderList', 2) # custom location
+    profile.set_preference('browser.download.manager.showWhenStarting', False)
+    profile.set_preference('browser.download.dir', downloadDir)
+    profile.set_preference('browser.helperApps.neverAsk.saveToDisk', "image/png,image/jpeg")
+    saveMVperChr(i)
+    os.system("rmdir  "+downloadDir)
 
