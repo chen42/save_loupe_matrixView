c16068fe34e371d554c1c19d613dc5c5c1e7cfed
Hao Chen <inbox@haochen.name>
2019-01-29 10:08:59 -0600

    minor improvement

--- overlay_svs.py
+++ overlay_svs.py
@@ -2,8 +2,10 @@ import os
 import pandas as pd
 from random import randint
 
-#based on pixel position of matrix view
+## set the chr here
+chrname="chr7"
 
+#based on pixel position of matrix view
 startPixel=144
 endPixel=1168
 pixelPerBase=(endPixel-startPixel)/500000
@@ -15,6 +17,9 @@ svcolor={"unk": "#dac29215",
 
 def drawbox(imgName, x1, x2, bp1, bp2, svtype):
     svImgName="sv_"+imgName
+# you can redo one specific image if needed:
+#    if svImgName!="sv_chr1_00036000001-00036500000_bn_both.png":
+#        return
     if not os.path.exists(svImgName):
         os.system("cp allPng/" +  img + " " +svImgName)
     text=svtype+" " + str(int(bp1))+"-"+str(int(bp2))
@@ -24,10 +29,11 @@ def drawbox(imgName, x1, x2, bp1, bp2, svtype):
     os.system("mogrify " +  svImgName +   " -fill \""+ col + "\" -draw \"rectangle " + str(x1) + ",0 " +  str(x2) + ",1024\" " +  " -pointsize 24 -fill \"#00000004\" -gravity southwest -draw \"rotate 315 text " + str(x) + "," + str(y) +"\'" + text + "\'\" " +  svImgName)
 
 
-df=pd.read_csv("chr20_svs.tab", delimiter="\t", header=None)
+filename=chrname+"_svs.tab"
+df=pd.read_csv(filename, delimiter="\t", header=None)
 for row, cols in df.iterrows():
     chrom=cols[1]
-    if chrom == "chr20": 
+    if chrom == chrname: #just checking to make sure
         svtype=cols[0]
         bpStart=cols[2]
         bpEnd=cols[3]
@@ -38,32 +44,14 @@ for row, cols in df.iterrows():
         chrstart=imgStart*500000+1
         chrend=(imgEnd+1)*500000
         if (imgEnd-imgStart==0):
-            img= "chr20_" +  str(chrstart).zfill(11) + "-" + str(chrend).zfill(11) +  "_bn_both.png"
-            print (str(bpStart) + "-" + str(bpEnd) )
+            img= chrname+ "_" +  str(chrstart).zfill(11) + "-" + str(chrend).zfill(11) +  "_bn_both.png"
+            print (chrname+ ":"+ str(bpStart) + "-" + str(bpEnd) )
             drawbox(img, pixelStart, pixelEnd, bpStart, bpEnd, svtype)
         elif (imgEnd-imgStart==1):
-            print (str(bpStart) + "-" + str(bpEnd) )
+            print (chrname+ ":"+ str(bpStart) + "-" + str(bpEnd) )
             chrend1=chrend-500000
-            img= "chr20_" +  str(chrstart).zfill(11) + "-" + str(chrend1).zfill(11) +  "_bn_both.png"
-            #print (img)
+            img= chrname+ "_" +  str(chrstart).zfill(11) + "-" + str(chrend1).zfill(11) +  "_bn_both.png"
             drawbox(img, pixelStart, endPixel, bpStart, bpEnd, svtype )
             chrstart1=chrend1+1
-            img= "chr20_" +  str(chrstart1).zfill(11) + "-" + str(chrend).zfill(11) +  "_bn_both.png"
-            #print (img)
-            drawbox(img, 0, pixelEnd, bpStart, bpEnd, svtype)
-
-'''
-# breakends 
-#df2=pd.read_csv("chr20_bnd.tab", delimiter="\t", header=None, dtype="float")
-df2=pd.read_csv("short.tab", delimiter="\t", header=None, dtype="float")
-for row, cols in df2.iterrows():
-    bp=cols[0]
-    img0=int(bp/500000)
-    pixelStart=startPixel+(bp-img0*500000)*pixelPerBase
-    pixelEnd=pixelStart+2
-    chrstart=img0*500000+1
-    chrend=chrstart+499999
-    img= "chr20_" +  str(chrstart).zfill(11) + "-" + str(chrend).zfill(11) +  "_bn_both.png"
-    print (img)
-    drawbox(img, pixelStart, pixelEnd, bp, bp)
-'''
+            img= chrname+ "_" +  str(chrstart1).zfill(11) + "-" + str(chrend).zfill(11) +  "_bn_both.png"
+            drawbox(img, startPixel, pixelEnd, bpStart, bpEnd, svtype)
